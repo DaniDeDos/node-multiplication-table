552c26b4e5edd9be7bd242ee8711b49f6cdd9013
leinad <leinad@gmail.com>
2024-12-17 17:30:55 +0100

    add colors

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,16 +1,17 @@
 const fs = require('fs')
+const colors = require('colors')
 
 const crearTabla = async (base = 5, listar = false) => {
     try {
         let salida = '';
 
         for (let i = 1; i <= 10; i++) {
-            salida += `${base} x ${i} = ${base * i}\n`
+            salida += `${base} ${'x'.red} ${i} ${'='.red} ${base * i}\n`
         }
 
         if (listar) {
             console.log('========================');
-            console.log('  Tabla del: ', base)
+            console.log('  Tabla del: ', colors.blue(base))
             console.log('========================');
 
             console.log(salida);
