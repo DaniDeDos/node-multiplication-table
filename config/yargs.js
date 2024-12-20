eff7a8853a73bc4340de523ed2be04a5ebfe4bfb
ihasashi <hasashih978@gmail.com>
2024-12-20 21:46:33 +0100

    change a type module

--- config/yargs.js
+++ config/yargs.js
@@ -1,28 +1,31 @@
-const argv = require('yargs')
-    .option('b', {
-        alias: 'base',
-        type: 'number',
-        demandOption: true,
-        describe: 'es la base de la tabla de multiplicar'
-    })
-    .option('h', {
-        alias: 'hasta',
-        type: 'number',
-        default: 10,
-        describe: 'limite hasta donde se va a generar la tabla'
-    })
-    .option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        default: false,
-        describe: 'muestra la tabla en persona'
-    })
-    .check((argv, options) => {
-        if (isNaN(argv.b)) {
-            throw 'la base tiene que ser un numero'
-        }
-        return true
-    })
-    .argv;
+import _yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+const yargs = _yargs(hideBin(process.argv));
 
-module.exports = argv;
+const argv = yargs
+  .option("b", {
+    alias: "base",
+    type: "number",
+    demandOption: true,
+    describe: "es la base de la tabla de multiplicar",
+  })
+  .option("h", {
+    alias: "hasta",
+    type: "number",
+    default: 10,
+    describe: "limite hasta donde se va a generar la tabla",
+  })
+  .option("l", {
+    alias: "listar",
+    type: "boolean",
+    default: false,
+    describe: "muestra la tabla en persona",
+  })
+  .check((argv, options) => {
+    if (isNaN(argv.b)) {
+      throw "la base tiene que ser un numero";
+    }
+    return true;
+  }).argv;
+
+export default argv ;
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,32 +1,27 @@
-const fs = require('fs')
-const colors = require('colors')
+import fs from "fs";
+import colors from "colors";
 
-const crearTabla = async (base = 5, hasta = 10, listar = false) => {
-    try {
-        let salida = '';
-        let consola = '';
+export const crearTabla = async (base = 5, hasta = 10, listar = false) => {
+  try {
+    let salida = "";
+    let consola = "";
 
-
-        for (let i = 1; i <= hasta; i++) {
-            salida += `${base} x ${i} = ${base * i}\n`
-            consola += `${base} ${'x'.red} ${i} ${'='.red} ${base * i}\n`
-        }
-
-        if (listar) {
-            console.log('========================');
-            console.log('     Tabla del: ', colors.blue(base))
-            console.log('========================');
-
-            console.log(consola);
-        }
-
-        fs.writeFileSync(`./salida/tabla-${base}.txt`, salida)
-        return (`tabla-${base}.txt`)
-    } catch (err) {
-        throw err
+    for (let i = 1; i <= hasta; i++) {
+      salida += `${base} x ${i} = ${base * i}\n`;
+      consola += `${base} ${"x".red} ${i} ${"=".red} ${base * i}\n`;
     }
-}
 
-module.exports = {
-    crearTabla
-}
+    if (listar) {
+      console.log("========================");
+      console.log("     Tabla del: ", colors.blue(base));
+      console.log("========================");
+
+      console.log(consola);
+    }
+
+    fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
+    return `tabla-${base}.txt`;
+  } catch (err) {
+    throw err;
+  }
+};
